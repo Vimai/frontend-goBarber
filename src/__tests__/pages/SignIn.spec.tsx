4a06e1b6774d5856c9861e2feae9850ceb9fcea2
Vinicius Imai <vinicius.imai@gmail.com>
2020-06-23 19:56:03 -0300

    começo do testes

--- src/__tests__/pages/SignIn.spec.tsx
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import SignIn from '../../pages/SignIn';
+
+const mockedHistoryPush = jest.fn();
 
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({ push: mockedHistoryPush }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
 describe('SingIn Page', () => {
   it('should be able to sign in', () => {
-    const { debug } = render(<SignIn />);
-    debug();
+    const { getByPlaceholderText } = render(<SignIn />);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const paswwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByPlaceholderText('Entrar');
+
+    fireEvent.change(emailField, {
+      target: { value: 'vinicius@gmail.com' },
+    });
+    fireEvent.change(paswwordField, {
+      target: { value: '123456' },
+    });
+
+    fireEvent.click(buttonElement);
+
+    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
   });
 });
